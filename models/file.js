cab64ca430e398365e6b96e040fc64c341056832
Nikitzu <nzi019000@gmail.com>
2016-01-26 18:56:40 +0300

    Problem with adding files to database!

--- models/file.js
+++ models/file.js
@@ -6,7 +6,6 @@ var mongoose = require('../libs/mongoose'),
 var schema = new Schema({
     username: {
         type: String,
-        unique: true,
         required: true
     },
     fileName: {
@@ -16,7 +15,12 @@ var schema = new Schema({
     fileText: {
         type: String,
         required: true
+    },
+    deleteHash: {
+        type: String,
+        required: true
     }
+
 });
 
 
@@ -54,21 +58,26 @@ schema.statics.findFile = function(username, fileName, callback) {
         }
     ], callback);
 };
-
+//Problem TODO
 schema.statics.addFile = function(username, fileName, fileText, callback) {
     var File = this;
+    console.log({username: username, fileName: fileName, fileText: fileText});
     async.waterfall([
         function(callback) {
+            console.log({username: username, fileName: fileName});
             File.findOne({username: username, fileName: fileName}, callback);
         },
         function(file, callback) {
+            console.log(file);
             if (file) {
+                console.log("Find!");
                 File.remove(file, callback);
             }
             var newFile = new File({username: username, fileName: fileName, fileText: fileText});
             newFile.save();
+            console.log(newFile);
         }
     ], callback);
 };
-
+//Problem TODO
 exports.File = mongoose.model('File', schema);
