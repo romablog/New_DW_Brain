01ffa18eba4971bfab5ef977dc136174699c2bcc
romablog <iromablog@gmail.com>
2016-01-26 22:14:05 +0300

    Save but not so good

--- routes/files.js
+++ routes/files.js
@@ -1,14 +1,14 @@
 var File = require('../models/file').File;
 
 exports.post = function(req, res, next){
-    File.addFile(res.locals.user.username,req.body.fileName, req.body.fileText)
+    File.addFile(res.locals.user.username,req.body.fileName, req.body.fileText);
 };
 
 
 exports.get = function(req, res, next) {
     File.allUsersFile(res.locals.user.username,function(fileNames){
         if (fileNames == "No Link"){
-            res.status(404).send("Fuck off!")
+            res.status(404).send("Fuck off!");
         }
         else{
             res.send(fileNames);
