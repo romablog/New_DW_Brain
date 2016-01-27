1b792c06f31c50370f29c84fd30f3f877089c64e
elugeo <elugeo@elugeo.elugeo>
2016-01-27 14:19:19 +0300

    редактирование и сохранение кода

--- public/sourse/sourseCode.js
+++ public/sourse/sourseCode.js
@@ -21,7 +21,7 @@ function init(){
         g_linebreaker = "\r";
     }
 
-    document.getElementById('edit_source').value = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
+   // document.getElementById('edit_source').value = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
     g_debugging = 1;
     init_memory();
     //update_memview();
