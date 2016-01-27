78f70521ef6a615fefc5c35b259b8379f8bba876
Nikitzu <nzi019000@gmail.com>
2016-01-27 13:57:15 +0300

    New!

--- public/sourse/sourseCode.js
+++ public/sourse/sourseCode.js
@@ -469,3 +469,20 @@ function change_button_caption(name, caption){
     var elm = document.getElementById(name);
     elm.value = caption;
 }
+
+//function insertTextAtCursor(text1, text2)
+//{
+//    if((document.selection))
+//    {
+//        document.form.message.focus();
+//        document.form.document.selection.createRange().text = text1+document.form.document.selection.createRange().text+text2;
+//    }
+//    else if(document.forms['form'].elements['message'].selectionStart !=undefined)
+//    {
+//        var element = document.forms['form'].elements['message'];
+//        var str = element.value;
+//        var start = element.selectionStart;
+//        var length = element.selectionEnd - element.selectionStart;
+//        element.value = str.substr(0, start)+text1+str.substr(start, length)+text2+str.substr(start+length);
+//    }
+//    else document.form.message.value+=text1+text2; ?}
--- public/view1/view1.js
+++ public/view1/view1.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var v1 = angular.module('brainApp.view1',[]);
+var v1 = angular.module('brainApp.view1',[
+    'infinite-scroll']);
 
 v1.controller('View1Controller', function($scope, SourceService) {
     console.log("Controller scope", SourceService.sourceFiles[0]);
